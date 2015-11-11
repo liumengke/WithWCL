a337bed860aa8a60ba5f8d338d8ef1ef9e8b1764
Qianfeng <qianfeng@LMK.local>
2015-11-11 16:52:11 +0800

    secondView

---
+++ WithWCL/WithWCL/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  WithWCL
+//
+//  Created by Qianfeng on 15/11/11.
+//  Copyright (c) 2015年 刘孟苛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ WithWCL/WithWCL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WithWCL
+//
+//  Created by Qianfeng on 15/11/11.
+//  Copyright (c) 2015年 刘孟苛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
